ce3ba9aa69d933c6a82a5dda5bcafa06b33ad147
varthadinesh <30522988+varthadinesh@users.noreply.github.com>
2023-11-24 19:51:20 +0530

    Add files via upload

---
+++ Frontend/src/Patientcomponents/components/appointments/Viewmyappointment.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Menu from "../Menu";
+import { Link, useLocation } from "react-router-dom";
+import Navbar from "../Navbar"
+
+
+function Viewmyappointment() {
+  return (
+    <>
+    <Navbar/>
+    <div className="d-flex">
+      <div className="col-2 p-0">
+        <Menu/>
+      </div>
+
+      <div className="col-10 p-0">
+       <h5 className="mt-4 mb-2 ms-2">Appointment Details</h5>
+        <div className="d-flex ms-2">
+        <div className="input-group w-25 m-1">
+            <input type="date" className="form-control" placeholder="Select Date"/>
+        </div>
+            <select className="form-select w-25  m-1">
+                <option>Select Appointment Type</option>
+                <option>Check up</option>
+                <option>tests</option>
+            </select>
+            <select className="form-select w-25  m-1">
+                <option>Select Center</option>
+                <option>Hyderabad</option>
+                <option>Bangalore</option>
+            </select>
+            <button className="btn btn-primary  m-1">Search</button>
+            
+        </div>
+        <table className="mt-4 ms-2 mb-4">
+          <tr className="m-5">
+            <th  className="success-t"><Link to="/upcomingappointments" className="text-decoration-none bg-success text-white p-2">Up Coming</Link></th>
+            <th  className="success-t" ><Link to="/previousappointments" className="text-decoration-none bg-dark text-white p-2">Previous</Link></th>
+          </tr>
+      </table>
+
+      <table className="table">
+       
+       <tr>
+         <th className="">Appointment Schedule Date</th>
+         <th className="">Appointment type</th>
+         <th className="">Center / City</th>
+        <th className=""  colspan="4">Actions</th>
+       </tr>
+               
+       <tr>
+       <td colSpan={4}> No Records available</td>
+       </tr>
+      </table>
+       
+      </div>
+    
+    </div>
+    </>
+  );
+}
+
+export default Viewmyappointment;
