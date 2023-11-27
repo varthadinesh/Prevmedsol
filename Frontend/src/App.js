cba82ece6dcefe74aec784b200710c422fcfe4d1
varthadinesh <30522988+varthadinesh@users.noreply.github.com>
2023-11-27 21:50:35 +0530

    Add files via upload

---
+++ Frontend/src/App.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddDoctor from './Admincomponents/components/users/AddDoctor';
+import Addpatient from './Admincomponents/components/users/AddPatient';
+import DashboardPage from './Admincomponents/components/Dashboard';
+import Addlab from './Admincomponents/components/centers/AddLab';
+import Addcenter from './Admincomponents/components/centers/AddCenter';
+import Addappointment from './Admincomponents/components/appointments/AddAppointment';
+import Addlabagent from './Admincomponents/components/users/AddLabagent';
+import Editcenter from './Admincomponents/components/centers/Editcenter';
+import Managecenters from './Admincomponents/components/centers/ManageCenters';
+import Managelabs from './Admincomponents/components/centers/Managelabs';
+import Managestate from './Admincomponents/components/locations/ManageState';
+import Managecity from './Admincomponents/components/locations/ManageCity';
+import Managelocation from './Admincomponents/components/locations/Managelocation';
+import Managepackage from './Admincomponents/components/packages/ManagePackage';
+import Addpackage from './Admincomponents/components/packages/Addpackage';
+import Patientappointment from './Admincomponents/components/appointments/PatientAppointment';
+import Labappointment from './Admincomponents/components/appointments/LabAppointment';
+import Register from './Admincomponents/components/loginpages/Register';
+import Adminpage from './Admincomponents/components/loginpages/Adminpage';
+import Frontoffice from './Admincomponents/components/loginpages/Frontoffice';
+import Doctor from './Admincomponents/components/loginpages/Doctor';
+import Patient from './Admincomponents/components/loginpages/Patient';
+import Lab from './Admincomponents/components/loginpages/Lab';
+import Superadminpage from './Admincomponents/components/loginpages/Superadminpage';
+import Doctors from './Admincomponents/components/accounts/Doctors';
+import Patients from './Admincomponents/components/accounts/Patients';
+import Labagents from './Admincomponents/components/accounts/Labagents';
+import Sitemanagers from './Admincomponents/components/accounts/Sitemanagers';
+import Invoice from './Admincomponents/components/payments/Invoice';
+import Viewpayments from './Admincomponents/components/payments/Viewpayments';
+import Home from './Admincomponents/components/Home';
+import Plans from './Admincomponents/components/plans';
+import Viewmyappointment from './Patientcomponents/components/appointments/Viewmyappointment';
+import Booknewappointment from './Patientcomponents/components/appointments/Booknewappointment';
+import Composemessage from './Patientcomponents/components/messages/Composemessage';
+import Inbox from './Patientcomponents/components/messages/Inbox';
+import Sentmessage from './Patientcomponents/components/messages/Sentmessage';
+import Previousappointments from './Patientcomponents/components/appointments/Previousappointments';
+import Myaccount from './Patientcomponents/components/settings/Myaccount';
+import Myprofile from './Patientcomponents/components/settings/Myprofile';
+import Pinvoice from './Patientcomponents/components/billing/Invoice';
+import Emailverification from './Admincomponents/components/loginpages/Emailverification';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter  basename="Prevmedsol">
+        <Routes>
+          {/* Admin components routes */}
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='plans' element={<Plans/>}></Route>
+          <Route path='register' element={<Register/>}></Route>
+          <Route path='emailverification' element={<Emailverification/>}></Route>
+          <Route path='adminpage' element={<Adminpage/>}></Route>
+          <Route path='frontoffice' element={<Frontoffice/>}></Route>
+          <Route path='doctor' element={<Doctor/>}></Route>
+          <Route path='patient' element={<Patient/>}></Route>
+          <Route path='lab' element={<Lab/>}></Route>
+          <Route path='superadminpage' element={<Superadminpage />}></Route>
+          <Route path='dashboardPage' element={<DashboardPage/>}></Route>
+          <Route path='adddoctor' element={<AddDoctor />}></Route>
+          <Route path='addpatient' element={<Addpatient />}></Route>
+          <Route path='addlabagent' element={<Addlabagent />}></Route>
+          <Route path='addlab' element={<Addlab/>}></Route>
+          <Route path='addcenter' element={<Addcenter/>}></Route>
+          <Route path='editcenter' element={<Editcenter/>}></Route>
+          <Route path='managecenters' element={<Managecenters/>}></Route>
+          <Route path='managelabs' element={<Managelabs/>}></Route>
+          <Route path='addappointment' element={<Addappointment/>}></Route>
+          <Route path='patientappointment' element={<Patientappointment/>}></Route>
+          <Route path='labappointment' element={<Labappointment/>}></Route>
+          <Route path='managestate' element={<Managestate/>}></Route>
+          <Route path='managecity' element={<Managecity/>}></Route>
+          <Route path='managelocation' element={<Managelocation/>}></Route>
+          <Route path='managepackage' element={<Managepackage/>}></Route>
+          <Route path='addpackage' element={<Addpackage/>}></Route>
+          <Route path='doctors' element={<Doctors/>}></Route>
+          <Route path='patients' element={<Patients/>}></Route>
+          <Route path='labagents' element={<Labagents/>}></Route>
+          <Route path='sitemanagers' element={<Sitemanagers/>}></Route>
+          <Route path='invoice' element={<Invoice/>}></Route>
+          <Route path='viewpayments' element={<Viewpayments/>}></Route>
+
+          {/* Patient components routes */}
+          <Route path='upcomingappointments' element={<Viewmyappointment/>}></Route>
+          <Route path='previousappointments' element={<Previousappointments/>}></Route>
+          <Route path='booknewappointment' element={<Booknewappointment/>}></Route>
+          <Route path='composemessage' element={<Composemessage/>}></Route>
+          <Route path='inbox' element={<Inbox/>}></Route>
+          <Route path='sentmessage' element={<Sentmessage/>}></Route>
+          <Route path='changepassword' element={<Myaccount/>}></Route>
+          <Route path='editprofile' element={<Myprofile/>}></Route>
+          <Route path='patientinvoice' element={<Pinvoice/>}></Route>
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/Patientcomponents/components/appointments/Viewmyappointment.js
@@ -0,0 +1,254 @@
+import React, { useState, useEffect } from "react";
+import Menu from "../Menu";
+import { Link, useLocation } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import axios from "axios";
+import Navbar from "../Navbar"
+import { useData } from "../../../Admincomponents/components/Datacontext";
+
+
+function Viewmyappointment() {
+  const { user } = useData();
+  const [info1, setInfo1] = useState([]);
+
+  let date = new Date().toLocaleDateString('sv');
+  console.log(typeof(date));
+
+  const patient = user.firstname + " " + user.lastname;
+  console.log(patient)
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/patientappointment")
+      .then((res) => {
+        // console.log(res.data);
+        res.data.data2.map((item) => {
+          console.log(item.patient, patient)
+            if (patient == item.patient) {
+              setInfo1((prev) => [...prev, item]);
+            }
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(info1);
+
+  return (
+    <>
+    <Navbar/>
+    <div className="d-flex">
+      <div className="col-2 p-0">
+        <Menu/>
+      </div>
+
+      <div className="col-10 p-0">
+       <h5 className="mt-4 mb-2 ms-2">Appointment Details</h5>
+        <div className="d-flex ms-2">
+        <div className="input-group w-25 m-1">
+            <input type="date" className="form-control" placeholder="Select Date"/>
+        </div>
+            <select className="form-select w-25  m-1">
+                <option>Select Appointment Type</option>
+                <option>Check up</option>
+                <option>tests</option>
+            </select>
+            <select className="form-select w-25  m-1">
+                <option>Select Center</option>
+                <option>Hyderabad</option>
+                <option>Bangalore</option>
+            </select>
+            <button className="btn btn-primary  m-1">Search</button>
+            
+        </div>
+
+        <Tabs defaultActiveKey="first" className="m-4">
+              <Tab eventKey="first" title="Upcoming">
+                <div>
+                  <table
+                    id="dynamic-table"
+                    className="table table-striped table-bordered table-hover dataTable no-footer"
+                    role="grid"
+                    aria-describedby="dynamic-table_info"
+                  >
+                    <thead>
+                      <tr role="row">
+                        <th
+                          className="sorting"
+                          tabIndex="0"
+                          aria-controls="dynamic-table"
+                          rowSpan="1"
+                          colSpan="1"
+                          aria-label="Appointment ID: activate to sort column ascending"
+                        >
+                          Appointment Schedule Date
+                        </th>
+                        <th
+                          className="sorting"
+                          tabIndex="0"
+                          aria-controls="dynamic-table"
+                          rowSpan="1"
+                          colSpan="1"
+                          aria-label="Patient Name: activate to sort column ascending"
+                        >
+                          Appointment Type
+                        </th>
+                        <th
+                          className="sorting"
+                          tabIndex="0"
+                          aria-controls="dynamic-table"
+                          rowSpan="1"
+                          colSpan="1"
+                          aria-label="Phone:activate to sort column ascending"
+                        >
+                          Center / City
+                        </th>
+                        <th
+                          className="hidden-480 sorting"
+                          tabIndex="0"
+                          aria-controls="dynamic-table"
+                          rowSpan="1"
+                          colSpan="1"
+                          aria-label="Gender: activate to sort column ascending"
+                        >
+                         Actions
+                        </th>
+                        <th
+                          className="hidden-480 sorting"
+                          tabIndex="0"
+                          aria-controls="dynamic-table"
+                          rowSpan="1"
+                          colSpan="1"
+                          aria-label="Appointment Date: activate to sort column ascending"
+                        >
+                         
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {info1.map((item, index) => {
+                        {if(item.date>date){
+                          return <tr key={index}>
+                          <td>{item.date} - {item.time}</td>
+                          <td>{item.appointment}</td>
+                          <td>{item.center}</td>
+                          <td>{}</td>
+                          <td>{}</td>
+                        </tr>
+                        }}
+                      })}
+                    </tbody>
+                  </table>
+                  
+                </div>
+              </Tab>
+              <Tab eventKey="second" title="Previous">
+              <div>
+                  <table
+                    id="dynamic-table"
+                    className="table table-striped table-bordered table-hover dataTable no-footer"
+                    role="grid"
+                    aria-describedby="dynamic-table_info"
+                  >
+                    <thead>
+                      <tr role="row">
+                        <th
+                          className="sorting"
+                          tabIndex="0"
+                          aria-controls="dynamic-table"
+                          rowSpan="1"
+                          colSpan="1"
+                          aria-label="Appointment ID: activate to sort column ascending"
+                        >
+                          Appointment Schedule Date
+                        </th>
+                        <th
+                          className="sorting"
+                          tabIndex="0"
+                          aria-controls="dynamic-table"
+                          rowSpan="1"
+                          colSpan="1"
+                          aria-label="Patient Name: activate to sort column ascending"
+                        >
+                          Appointment Type
+                        </th>
+                        <th
+                          className="sorting"
+                          tabIndex="0"
+                          aria-controls="dynamic-table"
+                          rowSpan="1"
+                          colSpan="1"
+                          aria-label="Phone:activate to sort column ascending"
+                        >
+                          Center / City
+                        </th>
+                        <th
+                          className="hidden-480 sorting"
+                          tabIndex="0"
+                          aria-controls="dynamic-table"
+                          rowSpan="1"
+                          colSpan="1"
+                          aria-label="Gender: activate to sort column ascending"
+                        >
+                         Actions
+                        </th>
+                        <th
+                          className="hidden-480 sorting"
+                          tabIndex="0"
+                          aria-controls="dynamic-table"
+                          rowSpan="1"
+                          colSpan="1"
+                          aria-label="Appointment Date: activate to sort column ascending"
+                        >
+                         
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {info1.map((item, index) => {
+                        {if(item.date<date){
+                          return <tr key={index}>
+                          <td>{item.date} - {item.time}</td>
+                          <td>{item.appointment}</td>
+                          <td>{item.center}</td>
+                          <td>{}</td>
+                          <td>{}</td>
+                        </tr>
+                        }}
+                      })}
+                    </tbody>
+                  </table>
+                  
+                </div>
+              </Tab>
+            </Tabs>
+        {/* <table className="mt-4 ms-2 mb-4">
+          <tr className="m-5">
+            <th  className="success-t"><Link to="/upcomingappointments" className="text-decoration-none bg-success text-white p-2">Up Coming</Link></th>
+            <th  className="success-t" ><Link to="/previousappointments" className="text-decoration-none bg-dark text-white p-2">Previous</Link></th>
+          </tr>
+      </table>
+
+      <table className="table">
+       
+       <tr>
+         <th className="">Appointment Schedule Date</th>
+         <th className="">Appointment type</th>
+         <th className="">Center / City</th>
+        <th className=""  colspan="4">Actions</th>
+       </tr>
+               
+       <tr>
+       <td colSpan={4}> No Records available</td>
+       </tr>
+      </table>
+        */}
+      </div>
+    
+    </div>
+    </>
+  );
+}
+
+export default Viewmyappointment;
